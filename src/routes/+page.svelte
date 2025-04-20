4bcd4e255b46577f5964753de957057ecd44d5a3
Posterizedsoul <beebekisme@gmail.com>
2025-04-20 14:30:26 +0545

    subtle homepage changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,16 @@
 <div class="fixed inset-0 -z-10 overflow-hidden">
 	<img
-		src="/backgroundremoved.png"
+		src="/background.png"
 		alt="Background"
-		class="absolute inset-0 h-full w-full object-contain origin-center rotate-90 "
+		class="absolute inset-0 h-full w-full object-contain origin-center scale-90"
 	/>
 </div>
 
 <div class="relative z-10 flex justify-center items-center h-screen">
 	<h1
-		class="text-7xl sm:text-8xl md:text-9xl lg:text-[10rem] font-normal text-center flex flex-col text-gray-800"
+		class="text-9xl sm:text-[10rem] md:text-[12rem] lg:text-[14rem] font-normal text-center flex flex-col text-gray-800 leading-none -translate-y-8"
 	>
-		<span>Bibek</span>
-		<span>Bhatta</span>
+		<span class="inline-block -translate-y-2 md:-translate-y-8">Bibek</span>
+		<span class="inline-block translate-y-2 md:translate-y-8">Bhatta</span>
 	</h1>
 </div>
